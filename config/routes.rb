2bdb4f0422578491c9626610f54dc3bef256cd3b
DonTom <dontom.developer@gmail.com>
2018-02-23 11:06:20 +0100

    Created model user and signup functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#index'
+  get 'users', to: 'users#index', as: 'users_root'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  resources :users
+  resources :sessions
+  
 end
