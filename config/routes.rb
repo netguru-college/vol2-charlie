5c76b43c2944b493cd1bfeef65c252ff6313b8c0
henrydabrow <henry.dabrow@gmail.com>
2018-02-27 16:37:12 +0100

    add 'Add Event' button

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do 
   root 'users#index'
   get 'users', to: 'users#index', as: 'users_root'
-  get 'signup', to: 'users#new', as: 'signup'
+  get 'create', to: 'users#new'
+  get 'newevent', to: 'events#new'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
 
