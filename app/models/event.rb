ef0b80c7b74edb036f4a8b2f0375d04458498087
Lidia Wisniewska <lidiawisnie@gmail.com>
2018-02-28 11:16:38 +0100

    Add geocoder to events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,7 @@
 class Event < ApplicationRecord
   has_many :participants
   has_many :users, through: :participants
+
+  geocoded_by :address
+  after_validation :geocode, if: ->(obj){ obj.address.present? and obj.address_changed? }
 end
