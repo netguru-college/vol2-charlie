c4b6e5e24fd785279b3f31922e67300af9d9b966
Lidia Wisniewska <lidiawisnie@gmail.com>
2018-02-28 11:51:20 +0100

    Add date and time to permitted params in events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -68,6 +68,6 @@ class EventsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def event_params
-    params.require(:event).permit(:name, :description, :latitude, :longitude, :address)
+    params.require(:event).permit(:name, :description, :latitude, :longitude, :address, :date, :time)
   end
 end
