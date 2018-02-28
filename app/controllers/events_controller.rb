e06d77e8a4f7aef4fc358ecd5ba4a044ba3ff6c7
Lidia Wisniewska <lidiawisnie@gmail.com>
2018-02-28 10:50:30 +0100

    Add address to permitted parameters for events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -68,6 +68,6 @@ class EventsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def event_params
-    params.require(:event).permit(:name, :description, :lat, :lng)
+    params.require(:event).permit(:name, :description, :lat, :lng, :address)
   end
 end
