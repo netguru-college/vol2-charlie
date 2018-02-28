87e72dee090f79654583dabc954b88bc41fceb52
Lidia Wisniewska <lidiawisnie@gmail.com>
2018-02-28 18:28:40 +0100

    Rubocop syntax correction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,11 +3,9 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :town])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name town])
   end
-
 end
