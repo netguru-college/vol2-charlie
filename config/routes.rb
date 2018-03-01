9301a25b5dc31c24f5e5284120b519de1606491a
Lidia Wisniewska <lidiawisnie@gmail.com>
2018-03-01 12:04:49 +0100

    Event sign up

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   resources :groups
-  resources :events
+  resources :events do
+  	get 'join', to: 'events#join_event'
+  end
   
   get 'user/my_events', to: 'users#my_events'
 end
