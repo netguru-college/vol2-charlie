d05951899aeb113839dc2eb8f7a2d9298377c8c5
henrydabrow <henry.dabrow@gmail.com>
2018-03-01 11:07:23 +0100

    Remove facebook from nav and add facebook to roots

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do 
-  devise_for :users
-
+  
   root to: 'users#index'
 
   resources :groups
   resources :events
   
   get 'user/my_events', to: 'users#my_events'
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+
+  get 'newevent', to: 'events#new'
 end
