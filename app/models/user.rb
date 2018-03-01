8eab00ff0a3da69a6b590ca3326b828cae8b4eea
Lidia Wisniewska <lidiawisnie@gmail.com>
2018-03-01 13:46:20 +0100

    Rubocop fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,8 +16,7 @@ class User < ApplicationRecord
 
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      
-      
+
       # user.image = auth.info.image # assuming the user model has an image
       # If you are using confirmable and the provider(s) you use validate emails,
       # uncomment the line below to skip the confirmation emails.
